2e0c343701b04b0d3c22d176ce340edfdd6f1e2d
Ajzu <arjun22singh009@gmail.com>
2020-01-06 15:44:32 +0530

    LeetCode -Single number puzzle

--- CompetitiveProgramming/LeetCode/LeetCode.cs
+++ CompetitiveProgramming/LeetCode/LeetCode.cs
@@ -14,13 +14,16 @@ namespace CompetitiveProgramming
             // int[] findNums = new int[] { 12, 345, 2, 6, 7896 };
             // FindNumbers(findNums);
 
-            //In Progress
-            string J = "aA";
-            string S = "aAAbbbbb";
+            //string J = "aA";
+            //string S = "aAAbbbbb";
             //NumJewelsInStones(J, S);
 
-            string toLowerCase = "LOVELY";
-            string test = ToLowerCase(toLowerCase);
+            //string toLowerCase = "LOVELY";
+            //string test = ToLowerCase(toLowerCase);
+
+            //In Progress
+            int[] nums = new int[] { 2, 2, 1 };///{ 4, 1, 2, 1, 2 };
+            int uniqueSinglenumber = SingleNumber2(nums);
         }
 
         // Function to remove duplicate 
@@ -106,6 +109,61 @@ namespace CompetitiveProgramming
         public string ToLowerCase(string str)
         {
             return str.ToLower();
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            int singleOne = 0;
+            
+            for (int i = 0; i < (nums.Length - 1); i++)
+            {
+                if (singleOne == 0)
+                {
+                    for (int j = (i + 1); j < nums.Length; j++)
+                    {
+                        if (nums[i] == nums[j])
+                        {
+                            singleOne = 0;
+                            int temp = nums[i + 1];
+                            nums[i + 1] = nums[j];
+                            nums[j] = temp;
+                            i++;
+                            break;
+                        }
+                        else
+                        {
+                            singleOne = nums[i];
+                        }
+                    } 
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(singleOne ==0)
+            {
+                singleOne = nums[nums.Length - 1];
+            }
+            return singleOne;
+        }
+
+        public int SingleNumber2(int[] nums)
+        {
+            int x = 0;
+            for (int i = 0; i < nums.Length; i++) x ^= nums[i];
+            return x;
+        }
+
+        public int SingleNumber3(int[] nums)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                res ^= nums[i];
+            }
+
+            return res;
         }
 
     }
