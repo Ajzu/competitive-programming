885ca40a6c950f71e53206798aa81bc25a1e4659
Ajzu <arjun22singh009@gmail.com>
2020-04-01 15:41:47 +0530

    CodeForces - Solved Bear and Big Brother
    CodeForces - Solved Bear and Big Brother. We can try other solutions maybe  we can try another mathemetical equation to solve the problem.

---
+++ CompetitiveProgramming/CodeForces/ProblemSet/BearAndBigBrother.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.CodeForces.ProblemSet
+{
+    public class BearAndBigBrother
+    {
+        /// <summary>
+        /// Edit below written Main2 to Main method.
+        /// Also, edit any other Main function to Main2 for running code from here.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            BearAndBigBrotherMethod();
+        }
+
+        /// <summary>
+        /// This is more of a mathemetical question where a and b values are changing by certain ratio.
+        /// One solution is to use while loop.
+        /// Other could be using the math equation to determine using some sort of mathemetical formula.
+        /// By this we will avoid loop in case of higher input numbers to save memory and time.
+        /// </summary>
+        private static void BearAndBigBrotherMethod()
+        {
+            string[] weights = Console.ReadLine().Split().ToArray();
+
+            int limak = Convert.ToInt32(weights[0]);
+            int bob = Convert.ToInt32(weights[1]);
+            int years = 0;
+
+            if (limak <= bob)
+            {
+                while (limak <= bob)
+                {
+                    bob = bob * 2;
+                    limak = limak * 3;
+                    years++;
+                }
+            }
+            Console.WriteLine(years);
+            Console.ReadLine();
+        }
+    }
+}
--- CompetitiveProgramming/CodeForces/ProblemSet/ProblemSetReadMe.txt
+++ CompetitiveProgramming/CodeForces/ProblemSet/ProblemSetReadMe.txt
@@ -1,6 +1,6 @@
 ﻿
 Solve ProblemSet are:
 	Wrong Subtraction - WrongSubtraction.cs
-
+	Bear and Big Brother- BearAndBigBrother.cs
 In Progress ProblemSet are:
 	In Search of an Easy Problem - InSearchOfAnEasyProblem.cs
