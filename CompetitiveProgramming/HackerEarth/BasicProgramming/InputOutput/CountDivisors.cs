948f4ba9dd560ba19b2621001a178a66458b0641
Arjun <arjun22singh009@gmail.com>
2020-10-31 19:11:12 +0530

    Problem Solving - HE - Basic Programming - Count Divisors
    Problem Solving - HE - Basic Programming - Count Divisors:
    1. Count Divisors - Solved - added test cases.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/CountDivisors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class CountDivisors
+    {
+        public static int BasicImplementation(int l, int r, int k)
+        {
+            int countDivisors = 0;
+
+            for(int i=l; i<=r; i++)
+            {
+                if(i%k==0)
+                {
+                    countDivisors++;
+                }
+            }
+            return countDivisors;
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int l = Convert.ToInt32(inputs[0]);
+            int r = Convert.ToInt32(inputs[1]);
+            int k = Convert.ToInt32(inputs[2]);
+
+            int countDivisors = 0;
+
+            for (int i = l; i <= r; i++)
+            {
+                if (i % k == 0)
+                {
+                    countDivisors++;
+                }
+            }
+            Console.WriteLine(countDivisors);
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/CountDivisorsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class CountDivisorsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCountDivisorsBasic()
+        {
+            int  expectedResult = 10;
+            int l = 1;
+            int r = 10;
+            int k = 1;
+
+            int actualResult = CountDivisors.BasicImplementation(1, 10, 1);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
