7ee7d3233716589ec7a4c85afbd62b623433cd63
Arjun <arjun22singh009@gmail.com>
2020-10-31 08:15:25 +0530

    Problem Solving - HE - Basic Programming - Movement
    Problem Solving - HE - Basic Programming - Movement:
    1. Solved with the Unit testing.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/Movement.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class Movement
+    {
+        public static int BasicImplementation()
+        {
+            int n = 26;
+            int[] steps = new int[] { 1, 2, 3, 4, 5 };
+
+            int totalSteps = 0;
+
+            for(int i=(steps.Length-1); i>=0; i--)
+            {
+                int divisor = steps[i];
+                int quotient = n / divisor;
+                int remainder = n % divisor;
+
+                if(quotient > 0)
+                {
+                    totalSteps += quotient;
+                    n = remainder;
+                }
+                
+                if(n==0)
+                {
+                    break;
+                }
+            }
+            return totalSteps;
+        }
+
+
+        /// <summary>
+        /// HackerEarthImplementation of Movement problem.
+        /// Solved - passed all the 7 test cases.
+        /// </summary>
+        public static void HackerEarthImplementation()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] steps = new int[] { 1, 2, 3, 4, 5 };
+
+            int totalSteps = 0;
+
+            for (int i = (steps.Length - 1); i >= 0; i--)
+            {
+                int divisor = steps[i];
+                int quotient = n / divisor;
+                int remainder = n % divisor;
+
+                if (quotient > 0)
+                {
+                    totalSteps += quotient;
+                    n = remainder;
+                }
+
+                if (n == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(totalSteps);
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/MovementTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class MovementTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMovementBasic()
+        {
+            int  expectedResult = 6;
+            int input = 26;
+
+            int actualResult = Movement.BasicImplementation();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
