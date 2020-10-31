dbed271436456588db773f9208b4c9518b909c64
Arjun <arjun22singh009@gmail.com>
2020-10-31 19:38:25 +0530

    Problem Solving - HE - Basic Programming - Roy And Profile Picture
    Problem Solving - HE - Basic Programming - Roy And Profile Picture:
    1. Solved - added test cases.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/RoyAndProfilePicture.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class RoyAndProfilePicture
+    {
+        public static string BasicImplementation()
+        {
+            string result = "";
+            int L = 180;
+            int N = 3;
+
+            for(int i=0; i<N; i++)
+            {
+                string[] WH = "640 480".Split(' ');
+                int W = Convert.ToInt32(WH[0]);
+                int H = Convert.ToInt32(WH[1]);
+
+                if (W < L || H < L)
+                {
+                    result = "UPLOAD ANOTHER";
+                }
+                else
+                {
+                    if(W == H)
+                    {
+                        result = "ACCEPTED";
+                    }
+                    else
+                    {
+                        result = "CROP IT";
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// HackerEarthImplementation
+        /// Solved - passed all the 7 test cases.
+        /// </summary>
+        public static void HackerEarthImplementation()
+        {
+            int L = Convert.ToInt32(Console.ReadLine());
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] WH = Console.ReadLine().Split(' ');
+                int W = Convert.ToInt32(WH[0]);
+                int H = Convert.ToInt32(WH[1]);
+
+                if (W < L || H < L)
+                {
+                    Console.WriteLine("UPLOAD ANOTHER");
+                }
+                else
+                {
+                    if (W == H)
+                    {
+                        Console.WriteLine("ACCEPTED");
+                    }
+                    else
+                    {
+                        Console.WriteLine("CROP IT");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/RoyAndProfilePictureTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class RoyAndProfilePictureTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestRoyAndProfilePictureBasic()
+        {
+            string  expectedResult = "CROP IT";
+
+            string actualResult = RoyAndProfilePicture.BasicImplementation();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
