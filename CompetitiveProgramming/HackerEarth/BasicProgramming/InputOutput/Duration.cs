e95b735f4487731db79338c1eb7e224cb478528d
Arjun <arjun22singh009@gmail.com>
2020-11-02 07:50:15 +0530

    Problem Solving - HE - Basic Programming - Duration
    Problem Solving - HE - Basic Programming - Duration:
    1. Solved - added unit test - passed 20 test cases.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/Duration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class Duration
+    {
+        public static string[] BasicImplementation(int N, string[] works)
+        {
+            string[] result = new string[N];
+
+            for(int i=0; i<N; i++)
+            {
+                string[] time = works[i].Split(' ');
+
+                int SH = Convert.ToInt32(time[0]);
+                int SM = Convert.ToInt32(time[1]);
+                int EH = Convert.ToInt32(time[2]);
+                int EM = Convert.ToInt32(time[3]);
+
+                int totalStartingMinutes = (SH * 60) + SM;
+                int totalEndingMinutes = (EH * 60) + EM;
+                int differenceMinutes = totalEndingMinutes - totalStartingMinutes;
+
+                int durationMinutes = differenceMinutes % 60;
+                int durationHours = differenceMinutes / 60;
+
+                result[i] = durationHours + " " + durationMinutes;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Solved- Passed all the 20 test cases
+        /// </summary>
+        public static void HackerEarthImplementation()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] time = Console.ReadLine().Split(' ');
+
+                int SH = Convert.ToInt32(time[0]);
+                int SM = Convert.ToInt32(time[1]);
+                int EH = Convert.ToInt32(time[2]);
+                int EM = Convert.ToInt32(time[3]);
+
+                int totalSM = (SH * 60) + SM;
+                int totalEM = (EH * 60) + EM;
+                int differenceM = totalEM - totalSM;
+
+                int durationM = differenceM % 60;
+                int durationH = differenceM / 60;
+
+                Console.WriteLine(durationH + " " + durationM);
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/DurationTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class DurationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestDurationBasic()
+        {
+            string[]  expectedResult = new string[] { "0 30", "5 41" };
+            int N = 2;
+            string[] works = new string[] { "1 44 2 14", "2 42 8 23" };
+            string[] actualResult = new string[N];
+
+            actualResult = Duration.BasicImplementation(N, works);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
