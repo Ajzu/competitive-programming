f912a0fc20fa70656297574083ddd3091daf86ba
Arjun <arjun22singh009@gmail.com>
2020-11-03 07:31:45 +0530

    Problem Solving - HE - BP - I Am Easy
    Problem Solving - HE - BP - I Am Easy:
    1. Solved - added unit test.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/IAmEasy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class IAmEasy
+    {
+        public static int[] BasicImplementation(int input)
+        {
+            int[] result = new int[10];
+
+            for(int i=1; i<11; i++)
+            {
+                result[i - 1] = input * i;
+            }
+
+            return result;
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(input * i);
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/IAmEasyTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class IAmEasyTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestIAmEasyBasic()
+        {
+            int[]  expectedResult = new int[] { 3,6,9,12,15,18,21,24,27,30 };
+            int N = 3;
+            int[] actualResult = new int[10];
+
+            actualResult = IAmEasy.BasicImplementation(N);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
