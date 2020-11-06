9f06e361b744ba9a3ac789cb8e9aa1759e1a263e
Arjun <arjun22singh009@gmail.com>
2020-11-06 07:46:31 +0530

    Problem Sovling - HE - BP - Doctor's Secret - Solved
    Problem Sovling - HE - BP - Doctor's Secret - Solved:
    1. Solved- Added unit test method.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/DoctorsSecret.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class DoctorsSecret
+    {
+        public static string BasicImplementation(string inputs)
+        {
+            string[] secretBook = inputs.Split(' ');
+            int length = Convert.ToInt32(secretBook[0]);
+            int pageNumber = Convert.ToInt32(secretBook[1]);
+
+            if(length<= 23 && pageNumber >= 500 && pageNumber <= 1000)
+            {
+                return "Take Medicine";
+            }
+            else
+            {
+                return "Don't Take Medicine";
+            }
+        }
+
+        public static void HackerEarthImplementation()
+        {   
+            string[] secretBook = Console.ReadLine().Split(' ');
+            int length = Convert.ToInt32(secretBook[0]);
+            int pageNumber = Convert.ToInt32(secretBook[1]);
+
+            if (length <= 23 && pageNumber >= 500 && pageNumber <= 1000)
+            {
+                Console.WriteLine("Take Medicine");
+            }
+            else
+            {
+                Console.WriteLine("Don't Take Medicine");
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/DoctorsSecretTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class DoctorsSecretTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestDoctorsSecretBasic()
+        {
+            string expectedResult = "Take Medicine";
+            string inputs = "27 1001";
+            string actualResult = "";
+
+            actualResult = DoctorsSecret.BasicImplementation( inputs );
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
