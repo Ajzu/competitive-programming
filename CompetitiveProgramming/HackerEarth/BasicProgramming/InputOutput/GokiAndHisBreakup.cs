a337902efe14b3743f2c0142b0fde9609737faab
Arjun <arjun22singh009@gmail.com>
2020-11-06 16:13:20 +0530

    ProblemSolving - HE- BP - GokiAndHisBreakup - Solved
    ProblemSolving - HE- BP - GokiAndHisBreakup - Solved:
    1. Solved- added unit tests.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/GokiAndHisBreakup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class GokiAndHisBreakup
+    {
+        public static string[] BasicImplementation(int N, int X, int[] Y)
+        {
+            string[] result = new string[N];
+
+            for(int i=0; i<N; i++)
+            {
+                if(Y[i] >= X)
+                {
+                    result[i] = "YES";
+                }
+                else
+                {
+                    result[i] = "NO";
+                }
+            }
+
+            return result;
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int Y = Convert.ToInt32(Console.ReadLine());
+
+                if (Y >= X)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/GokiAndHisBreakupTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class GokiAndHisBreakupTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestGokiAndHisBreakupBasic()
+        {
+            string[] expectedResult = new string[] { "YES", "YES", "NO", "YES", "NO" };
+
+            int N = 5;
+            int X = 100;
+            int[] Y = new int[] { 110, 130, 90, 100, 45 };
+
+            string[] actualResult = new string[N];
+
+            actualResult = GokiAndHisBreakup.BasicImplementation( N, X, Y );
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
