33cb10b9ed19fb98716ec78320d6fb81e061aeb9
Arjun <arjun22singh009@gmail.com>
2020-11-06 16:33:13 +0530

    Problem Solving - HE-BP-Divisibility - Solved
    Problem Solving - HE-BP-Divisibility - Solved:
    1. Solved - added unit test.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/Divisibility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class Divisibility
+    {
+        public static string BasicImplementation(int N, string numbers)
+        {
+            string lastItem = numbers.Substring((numbers.Length - 1), 1);
+
+            if(lastItem == "0")
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+
+        public static string HackerEarthImplementation()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            string numbers = Console.ReadLine();
+
+            string lastItem = numbers.Substring((numbers.Length - 1), 1);
+
+            if (lastItem == "0")
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/DivisibilityTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class DivisibilityTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestDivisibilityBasic()
+        {
+            string  expectedResult = "No";
+            int N = 5;
+            string numbers = "85 25 65 21 84";
+
+            string actualResult = Divisibility.BasicImplementation(N, numbers);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
