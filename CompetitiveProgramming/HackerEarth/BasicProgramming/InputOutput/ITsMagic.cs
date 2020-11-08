05db344b9ccf0f62868bf29f7665b0696c8ce039
Arjun <arjun22singh009@gmail.com>
2020-11-08 07:48:53 +0530

    Problem Solving - HE - BP - It's Magic - Solved
    Problem Solving - HE - BP - It's Magic - Solved:
    1. Problem Solving - HE - BP - It's Magic - Solved.
    2. Tricky one - 2 iterations of submission to understand the question.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/ITsMagic.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class ITsMagic
+    {
+        public static int BasicImplementation(int N, string inputs)
+        {
+            string[] A = inputs.Split(' ');
+            int smallestIndex = -1;
+            long smallestItem = 1000000000;
+
+            long sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                sum += Convert.ToInt64(A[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                long temp = Convert.ToInt64(A[i]);
+
+                if (((sum - temp) % 7 == 0) && (temp < smallestItem))
+                {
+                    smallestItem = temp;
+                    smallestIndex = i;
+                }
+            }
+
+            return smallestIndex;            
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            string[] A = Console.ReadLine().Split(' ');
+
+            int smallestIndex = -1;
+            long smallestItem = 1000000000;
+            long sum = 0;
+
+            for(int i=0; i<N; i++)
+            {
+                sum += Convert.ToInt32(A[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                int temp = Convert.ToInt32(A[i]);
+
+                if (((sum -temp) % 7 == 0) && (temp < smallestItem))
+                {
+                    smallestItem = temp;
+                    smallestIndex = i;
+                }
+            }
+
+            Console.WriteLine(smallestIndex);
+        }
+
+        public static void Discussion()
+        {
+            string totalnumbers = Console.ReadLine();
+            string[] numbers = Console.ReadLine().ToString().Split(' ');
+
+            int sum = 0;
+            long minNum = 1000000000000, minIndex = -1;
+
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                sum += int.Parse(numbers[j]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if ((sum - int.Parse(numbers[i])) % 7 == 0 && int.Parse(numbers[i]) < minNum)
+                {
+                    minNum = int.Parse(numbers[i]);
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine(minIndex);
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/ITsMagicTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class ITsMagicTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestITsMagicBasic()
+        {
+            int expectedResult = 1;
+            int N = 100;
+            string numbers = "14 7 8 2 4";
+            string numbers2 ="290267 543718 152065 36958 513757 177467 242991 350675 953587 2959 791293 804018 858084 13158 47417 736925 231101 110680 644174 327034 139929 578888 920458 773574 433483 2544 121149 455734 32135 211876 403294 178448 171656 844908 467918 757493 383815 33552 832540 652097 38529 306142 659172 313415 353482 225856 75059 416520 62819 821080 206583 686447 217176 172935 91743 49898 944080 209288 588392 143368 103781 883998 569614 805835 798227 76174 884758 406719 173453 411231 12981 805198 140596 297707 986053 353198 75768 882624 184508 189976 570436 305100 318421 824387 408319 547365 328846 251777 756456 718935 543525 552642 69100 34364 353197 576434 115576 227224 146222 130404";
+
+            int actualResult = ITsMagic.BasicImplementation(N, numbers2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
