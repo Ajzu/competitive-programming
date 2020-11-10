f4f1af81c08ffcebe01c2c57cf7595956d50c636
Arjun <arjun22singh009@gmail.com>
2020-11-10 15:09:14 +0530

    Problem Solving - HE - BP - NumberOfSteps - Partially Solved
    Problem Solving - HE - BP - NumberOfSteps - Partially Solved:
    1. The question is tricky and better logic needs to be implemented.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/NumberOfSteps.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class NumberOfSteps
+    {
+        public static int BasicImplementation(int n, int[]A, int []B)
+        {
+            int smallestNum = A[0];
+            int numberOfSteps = 0;
+
+            for(int i=1; i<n; i++)
+            {
+                if(smallestNum > A[i])
+                {
+                    smallestNum = A[i];
+                }
+            }
+
+            for(int i=0; i<n; i++)
+            {
+                int steps = 0;
+
+                if (A[i] >= smallestNum && A[i] > B[i])
+                {
+                    while(A[i] > smallestNum)
+                    {
+                        A[i] = A[i] - B[i];
+                        steps++;
+
+                        if (A[i] == smallestNum)
+                        {
+                            break;
+                        }
+                        else if(A[i] <smallestNum)
+                        {
+                            steps--;
+                            break;
+                        }                        
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+
+                numberOfSteps += steps;
+            }
+
+            if (numberOfSteps == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return numberOfSteps;
+            }            
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] A = Console.ReadLine().Split(' ');
+            string[] B = Console.ReadLine().Split(' ');
+
+            int smallestNum = Convert.ToInt32(A[0]);
+            int numberOfSteps = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                int currentNum = Convert.ToInt32(A[i]);
+                if (smallestNum > currentNum)
+                {
+                    smallestNum = currentNum;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int steps = 0;
+                int a = Convert.ToInt32(A[i]);
+                int b = Convert.ToInt32(B[i]);
+
+                if (a >= smallestNum && a > b)
+                {
+                    while (a > smallestNum)
+                    {
+                        a = a - b;
+                        steps++;
+
+                        if (a == smallestNum)
+                        {
+                            break;
+                        }
+                        else if (a < smallestNum)
+                        {
+                            steps--;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    //Console.WriteLine("-1");
+                    steps = -1;
+                    break;
+                }
+
+                if (steps == -1)
+                {
+                    numberOfSteps = 0;
+                    break;
+                }
+                else
+                {
+                    numberOfSteps += steps;
+                }
+            }
+
+            if (numberOfSteps == 0)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine(numberOfSteps);
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/NumberOfStepsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class NumberOfStepsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBasicImplementation1()
+        {
+            int expectedResult = 8;
+            int N = 5;
+            int[] A = new int[] { 5, 7, 10, 5, 15 };
+            int[] B = new int[] { 2, 2, 1, 3, 5 };
+
+            int actualResult = NumberOfSteps.BasicImplementation(N, A, B);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestBasicImplementation2()
+        {
+            int expectedResult = -1;
+            int N = 2;
+            int[] A = new int[] { 5, 6 };
+            int[] B = new int[] { 4, 3 };
+
+            int actualResult = NumberOfSteps.BasicImplementation(N, A, B);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
