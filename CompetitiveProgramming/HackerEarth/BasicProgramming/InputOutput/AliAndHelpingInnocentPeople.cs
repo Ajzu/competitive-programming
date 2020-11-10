32e59a09930fc7c080c7bab187c397d5a4520da8
Arjun <arjun22singh009@gmail.com>
2020-11-10 07:51:32 +0530

    Problem Solving - HE - BP - AliAndHelpingInnocentPeople - Solved
    Problem Solving - HE - BP - AliAndHelpingInnocentPeople - Solved:
    1: Added 3 unit test methods to test multiple scenarios.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/AliAndHelpingInnocentPeople.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class AliAndHelpingInnocentPeople
+    {
+        public static string BasicImplementation(string inputs)
+        {
+            int length = 9;
+            string format = "DDXDDD-DD";
+            char[] vowels = new char[] { 'A', 'E', 'I', 'O', 'U', 'Y' };
+            bool foundVowel = false;
+
+            foreach(char ch in vowels)
+            {
+                if(ch == inputs[2])
+                {
+                    foundVowel = true;
+                    break;
+                }
+            }
+
+            if(foundVowel == true)
+            {
+                return "invalid";
+            }
+            else
+            {
+                int sum12 = Convert.ToInt32(inputs[0]) + Convert.ToInt32(inputs[1]);
+
+                int sum45 = Convert.ToInt32(inputs[3]) + Convert.ToInt32(inputs[4]);
+                int sum56 = Convert.ToInt32(inputs[4]) + Convert.ToInt32(inputs[5]);
+
+                int sum89 = Convert.ToInt32(inputs[7]) + Convert.ToInt32(inputs[8]);
+
+                if( (sum12 % 2==0) && (sum45 % 2 == 0) && (sum56 % 2 == 0) && (sum89 % 2 == 0) )
+                {
+                    return "valid";
+                }
+                else
+                {
+                    return "invalid";
+                }
+            }
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            string inputs = Console.ReadLine();
+
+            char[] vowels = new char[] { 'A', 'E', 'I', 'O', 'U', 'Y' };
+            bool foundVowel = false;
+
+            foreach (char ch in vowels)
+            {
+                if (ch == inputs[2])
+                {
+                    foundVowel = true;
+                    break;
+                }
+            }
+
+            if (foundVowel == true)
+            {
+                Console.WriteLine("invalid");
+            }
+            else
+            {
+                int sum12 = Convert.ToInt32(inputs[0]) + Convert.ToInt32(inputs[1]);
+                int sum45 = Convert.ToInt32(inputs[3]) + Convert.ToInt32(inputs[4]);
+                int sum56 = Convert.ToInt32(inputs[4]) + Convert.ToInt32(inputs[5]);
+                int sum89 = Convert.ToInt32(inputs[7]) + Convert.ToInt32(inputs[8]);
+
+                if ((sum12 % 2 == 0) && (sum45 % 2 == 0) && (sum56 % 2 == 0) && (sum89 % 2 == 0))
+                {
+                     Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/AliAndHelpingInnocentPeopleTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class AliAndHelpingInnocentPeopleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBasicImplementation1()
+        {
+            string expectedResult = "invalid";
+            string input = "12X345-67";
+
+            string actualResult = AliAndHelpingInnocentPeople.BasicImplementation(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestBasicImplementation2()
+        {
+            string expectedResult = "valid";
+            string input = "11X246-68";
+
+            string actualResult = AliAndHelpingInnocentPeople.BasicImplementation(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestBasicImplementation3()
+        {
+            string expectedResult = "invalid";
+            string input = "11A246-68";
+
+            string actualResult = AliAndHelpingInnocentPeople.BasicImplementation(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
