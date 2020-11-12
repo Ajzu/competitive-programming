4d213f4c223a428dcd076eafac699e1f035221b7
Arjun <arjun22singh009@gmail.com>
2020-11-12 08:48:04 +0530

    Problem Solving- HE - BP - BookOfPotionMaking - Solved
    Problem Solving- HE - BP - BookOfPotionMaking - Solved:
    1. Added 3 unit test cases.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/BookOfPotionMaking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class BookOfPotionMaking
+    {
+        public static string BasicImplementation(string digitCode)
+        {
+            if(digitCode.Length != 10)
+            {
+                return "Illegal ISBN";
+            }
+            else
+            {
+                int totalSum = 0;
+
+                for(int i=1; i<11; i++)
+                {
+                    int sum = i * Convert.ToInt32(digitCode.Substring((i-1),1));
+                    totalSum += sum;
+                }
+
+                if(totalSum %11 == 0 && totalSum >10)
+                {
+                    return "Legal ISBN";
+                }
+                else
+                {
+                    return "Illegal ISBN";
+                }
+            }
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            string digitCode = Console.ReadLine();
+            if (digitCode.Length != 10)
+            {
+                Console.WriteLine( "Illegal ISBN" );
+            }
+            else
+            {
+                int totalSum = 0;
+
+                for (int i = 1; i < 11; i++)
+                {
+                    int sum = i * Convert.ToInt32(digitCode.Substring((i - 1), 1));
+                    totalSum += sum;
+                }
+
+                if (totalSum % 11 == 0 && totalSum > 10)
+                {
+                    Console.WriteLine( "Legal ISBN" );
+                }
+                else
+                {
+                    Console.WriteLine( "Illegal ISBN" );
+                }
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/BookOfPotionMakingTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class BookOfPotionMakingTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBasicImplementation()
+        {
+            string expectedResult = "Legal ISBN";
+            string digitCode = "1401601499";
+
+            string actualResult = BookOfPotionMaking.BasicImplementation(digitCode);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestBasicImplementation2()
+        {
+            string expectedResult = "Illegal ISBN";
+            string digitCode = "14016014991";
+
+            string actualResult = BookOfPotionMaking.BasicImplementation(digitCode);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestBasicImplementation3()
+        {
+            string expectedResult = "Illegal ISBN";
+            string digitCode = "0000000000";
+
+            string actualResult = BookOfPotionMaking.BasicImplementation(digitCode);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
