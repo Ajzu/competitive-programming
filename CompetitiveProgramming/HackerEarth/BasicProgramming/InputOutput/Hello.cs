410b58fa18ad61ae37ee5d932898e12eac1c92a2
Arjun <arjun22singh009@gmail.com>
2020-11-12 08:19:36 +0530

    Problem Solving - HE - BP - Hello - Solved
    Problem Solving - HE - BP - Hello - Solved:
    1. Added unit tests.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class Hello
+    {
+        public static string  BasicImplementation()
+        {
+            return "Hello Kirti";
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            Console.WriteLine("Hello Kirti");
+        }
+
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/HelloTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class HelloTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBasicImplementation()
+        {
+            string expectedResult = "Hello Kirti";
+            string actualResult = Hello.BasicImplementation();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
