f722f7538513746a8ee0534adeba9935f720e7cc
Arjun <arjun22singh009@gmail.com>
2020-11-15 19:50:07 +0530

    Problem Solving - HE - BP - Cipher - Partially Solved
    Problem Solving - HE - BP - Cipher - Partially Solved:
    1. Partially solved - some large keys division and difference function is creating issues.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/Cipher.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class Cipher
+    {
+        public static string BasicImplementation(string S, int key)
+        {
+            string encrypt = "";
+            
+            //int sample = Difference(150, 122);
+
+            for (int i=0; i< S.Length; i++)
+            {
+                int currentItem = 0;
+
+                if (isDigits(S[i]))
+                {
+                    currentItem = Convert.ToInt32(S[i].ToString());
+                }
+                else
+                {
+                    currentItem = Convert.ToInt32(S[i]);
+                }                
+
+                if(currentItem >=65 && currentItem <= 90)
+                {
+                    currentItem += key;
+
+                    if (currentItem > 90)
+                    {
+                        int difference = Difference(currentItem, 90);
+                        //int difference = currentItem - 90;
+                        currentItem = 64 + difference;
+                    }
+
+                    encrypt += ((char)currentItem).ToString();
+                }
+                else if(currentItem >=97 && currentItem <= 122)
+                {
+                    currentItem += key;
+
+                    if (currentItem > 122)
+                    {
+                        int difference = Difference(currentItem, 122);
+                        //int difference = currentItem - 122;
+                        currentItem = 96 + difference;
+                    }
+
+                    encrypt += ((char)currentItem).ToString();
+                }
+                else if(currentItem >=0 && currentItem <= 9)
+                {
+                    currentItem += key;
+
+                    if (currentItem > 9)
+                    {
+                        int difference = Difference(currentItem, 9);
+                        //int difference = currentItem -9;
+                        currentItem = difference -1;
+                    }
+
+                    encrypt += currentItem.ToString();
+                }
+                else
+                {
+                    encrypt += S[i];
+                }
+            }
+
+            return encrypt;
+        }
+
+
+        public static void HackerEarthImplementation()
+        {
+            string S = Console.ReadLine();
+            int key = Convert.ToInt32(Console.ReadLine());
+            string encrypt = "";
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                int currentItem = 0;
+
+                if (isDigits(S[i]))
+                {
+                    currentItem = Convert.ToInt32(S[i].ToString());
+                }
+                else
+                {
+                    currentItem = Convert.ToInt32(S[i]);
+                }
+
+                if (currentItem >= 65 && currentItem <= 90)
+                {
+                    currentItem += key;
+
+                    if (currentItem > 90)
+                    {
+                        int difference = Difference(currentItem, 90);
+                        currentItem = 64 + difference;
+                    }
+
+                    encrypt += ((char)currentItem).ToString();
+                }
+                else if (currentItem >= 97 && currentItem <= 122)
+                {
+                    currentItem += key;
+
+                    if (currentItem > 122)
+                    {
+                        int difference = Difference(currentItem, 122);
+                        currentItem = 96 + difference;
+                    }
+
+                    encrypt += ((char)currentItem).ToString();
+                }
+                else if (currentItem >= 0 && currentItem <= 9)
+                {
+                    currentItem += key;
+
+                    if (currentItem > 9)
+                    {
+                        int difference = Difference(currentItem, 9);
+                        currentItem = difference - 1;
+                    }
+
+                    encrypt += currentItem.ToString();
+                }
+                else
+                {
+                    encrypt += S[i];
+                }
+            }
+
+            Console.WriteLine(encrypt);
+        }
+
+        public static bool isDigits(char s)
+        {
+            if ((s ^ '0') > 9)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int Difference(int currentItem , int maxLimit)
+        {
+            int difference = currentItem % maxLimit;
+
+            return difference;
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/CipherTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class CipherTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCipherBasic()
+        {
+            string expectedResult = "Epp-gsrzsCw-3-fi:Epivx5.";
+
+            string inputs = "All-convoYs-9-be:Alert1."; //"Y91.";// "All-convoYs-9-be:Alert1.";
+            int key = 4;
+            
+
+            string actualResult = Cipher.BasicImplementation( inputs, key );
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
