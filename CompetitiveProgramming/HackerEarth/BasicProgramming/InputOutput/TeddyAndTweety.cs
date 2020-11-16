5c176a15544f9728042db2bf38252d5e384fb5a3
Arjun <arjun22singh009@gmail.com>
2020-11-16 07:15:06 +0530

    Problem Solving - HE - BP - TeddyAndTweety - Solved
    Problem Solving - HE - BP - TeddyAndTweety - Solved:
    1. Added unit test case.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/TeddyAndTweety.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class TeddyAndTweety
+    {
+        public static string BasicImplementation(int N)
+        {
+            if(N %3 == 0)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 3 == 0)
+            {
+                Console.WriteLine( "YES" );
+            }
+            else
+            {
+                Console.WriteLine( "NO" );
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/TeddyAndTweety.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class TeddyAndTweetyTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBasic()
+        {
+            string expectedResult = "YES";
+            int inputs = 6;            
+
+            string actualResult = TeddyAndTweety.BasicImplementation( inputs );
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
