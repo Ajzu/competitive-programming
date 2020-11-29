91003daf65e2139470258658e805647e921bfb83
Arjun <arjun22singh009@gmail.com>
2020-11-29 08:09:32 +0530

    HackerEarth - BP - VC Paris - Partially Solved
    HackerEarth - BP - VC Paris - Partially Solved:
    1. Added unit test methods.

---
+++ CompetitiveProgramming/HackerEarth/BasicProgramming/InputOutput/VCPairs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgramming.HackerEarth.BasicProgramming
+{
+    public class VCPairs
+    {
+        public static int[] BasicImplementation(int testCases, int[] lengths, string[] inputs)
+        {
+            int[] result = new int[testCases];
+
+            for (int j = 0; j < testCases; j++)
+            {
+                int occurences = 0;
+
+                for (int i = 1; i < (inputs[j].Length); i++)
+                {   
+                    char currentitem = inputs[j][i];
+                    char previousItem = inputs[j][i];
+
+                    if (currentitem == 'a' || currentitem == 'e' || currentitem == 'i' || currentitem == 'o' || currentitem == 'u')
+                    {
+                        if (previousItem != 'a' || previousItem != 'e' || previousItem != 'i' || previousItem != 'o' || previousItem != 'u')
+                        {
+                            occurences++;
+                        }
+                    }
+                }
+                result[j] = occurences;
+            }
+
+            return result;
+        }
+
+        public static void HackerEarthImplementation()
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 0; j < testCases; j++)
+            {
+                int occurences = 0;
+                int length = Convert.ToInt32(Console.ReadLine());
+                string SS = Console.ReadLine();
+
+                for (int i = 1; i < (SS.Length); i++)
+                {
+                    char currentitem = SS[i];
+                    char previousItem = SS[i -1];
+
+                    if (currentitem == 'a' || currentitem == 'e' || currentitem == 'i' || currentitem == 'o' || currentitem == 'u')
+                    {
+                        if (previousItem != 'a' || previousItem != 'e' || previousItem != 'i' || previousItem != 'o' || previousItem != 'u')
+                        {
+                            occurences++;
+                        }
+                    }
+                }
+                Console.WriteLine(occurences);
+            }
+        }
+    }
+}
---
+++ CompetitiveProgrammingUnitTest/HackerEarth/BasicProgramming/InputOutput/VCPairsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using CompetitiveProgramming.HackerEarth.BasicProgramming;
+
+namespace CompetitiveProgrammingUnitTest.HackerEarth
+{
+    public class VCParisTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestBasic()
+        {
+            int[] expectedResult = new int[] { 3, 1, 0 };
+
+            int testCases = 3;
+            int[] lengths = new int[] { 6, 3, 1 };
+            string[] inputs = new string[] { "bazeci", "abu", "o" };
+
+            int[] actualResult = VCPairs.BasicImplementation(testCases, lengths, inputs);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
